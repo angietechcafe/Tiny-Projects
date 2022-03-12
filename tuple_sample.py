aa6007c73bccd4146f70ea4d1c629563417ddbeb
Angie <sacconeangela12@gmail.com>
2022-03-12 18:58:50 -0500

    Create tuple_sample.py

---
+++ tuple_sample.py
@@ -0,0 +1,9 @@
+# Goal: turn my fruit list into a tuple 
+fruits = ["apple","orange","grape","strawberry","banana"]
+# update the fruit list first by adding cherries.
+fruits[3] = "cherries"
+# use tuple() to create fruits list into a tuple.
+tuple(fruits)
+#output would be: apple, orange, grape, cherries, banana
+# Make a mini tuple out of a tuple by slicing it with an index. Then print it out. 
+print(fruits[0:2]) 
