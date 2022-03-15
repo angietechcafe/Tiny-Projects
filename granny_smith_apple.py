0f968bd0737cd869e6b3b4c8f3f40f6482a3ce8e
Angie <sacconeangela12@gmail.com>
2022-03-14 22:56:07 -0400

    Create granny_smith_apple.py

---
+++ granny_smith_apple.py
@@ -0,0 +1,3 @@
+# This python program uses the built-in function, input().
+apple = input('What is your favorite type of apple?\n').strip().lower()
+print('My favorite type of apple is %s.' % apple)
