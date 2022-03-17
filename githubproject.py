e9814a846695f8ebd3461183f0645023db938c4d
Angie <sacconeangela12@gmail.com>
2022-03-16 22:29:08 -0400

    Update githubproject.py

--- githubproject.py
+++ githubproject.py
@@ -1,9 +1,13 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as beausoup
 
 # user input
 Github_link = input("Paste Github user link here: " )
 # -> angieintech
-# HTTP get request to get the github url link
-r = requests.get(Github_link)
+# HTTP GET request to get the github url link
+req = requests.get(Github_link)
+# Web scraping the image begins here
+soup = beausoup(req.content, 'html.parser')
+# Use the image tag to find a certain image on Github.
+# certain_image = soup.find('img',{})
 
