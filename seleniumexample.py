62a25cf5691a5668725624c3c9d16be3241d454b
Angie <sacconeangela12@gmail.com>
2022-04-01 21:25:13 -0400

    Update seleniumexample.py

--- seleniumexample.py
+++ seleniumexample.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 # use HTTP requests to get info
 import requests 
+import magic
 import time
 # find_element_by_xpath
 # driver.find_element_by_xpath('//paste xpathfrom the website in here /input')
