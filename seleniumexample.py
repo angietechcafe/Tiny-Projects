cba5c848b8d33c0b5498d0f1f1c330065592df10
Angie <sacconeangela12@gmail.com>
2022-04-06 22:12:04 -0400

    use explicit wait and click on the image.

--- seleniumexample.py
+++ seleniumexample.py
@@ -10,11 +10,8 @@ import time
 # find_element_by_xpath
 # driver.find_element_by_xpath('//paste xpathfrom the website in here /input')
 
-# wait 10 seconds using explicit wait
+# wait 20 seconds using explicit wait & use xpath to click on the image
 driver = webdriver.Firefox()
-try:
-  #  element = WebDriverWait(driver, 10).until(
-  #     EC.presence_of_element_located((By.ID, "#"))
-  # )
+WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH,'//input[@type="image"][@src="/images/#"]'))).click()
 finally:
     driver.quit()
