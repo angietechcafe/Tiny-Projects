015aa81e546768a6411e155a83621cdf1b30a22c
Angie <sacconeangela12@gmail.com>
2022-04-06 22:15:13 -0400

    Update and rename githubproject.py to kittens_google_link.py

--- kittens_google_link.py
+++ kittens_google_link.py
@@ -2,10 +2,10 @@ import requests
 from bs4 import BeautifulSoup as beausoup
 
 # user input
-Github_link = input("Paste Github user link here: " )
+kittens_google_link = input("Paste kittens google link here: " )
 # -> angieintech
 # HTTP GET request to get the github url link
-req = requests.get(Github_link)
+req = requests.get(kittens_google_link)
 # Web scraping the image begins here
 soup = beausoup(req.content, 'html.parser')
 # Use the image tag to find a certain image on Github.
