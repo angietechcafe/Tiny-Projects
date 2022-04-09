39c2f378cd1870ba87b6c152fea6c776133f65d0
Angie <sacconeangela12@gmail.com>
2022-04-09 00:01:04 -0400

    Update kittens_google_link.py

--- kittens_google_link.py
+++ kittens_google_link.py
@@ -3,11 +3,13 @@ from bs4 import BeautifulSoup as beausoup
 
 # user input
 kittens_google_link = input("Paste kittens google link here: " )
-# -> angieintech
+
 # HTTP GET request to get the github url link
 req = requests.get(kittens_google_link)
+
 # Web scraping the image begins here
 soup = beausoup(req.content, 'html.parser')
+
 # Use the image tag to find a certain image on Github.
 # certain_image = soup.find('img',{'something like an avatar?': 'alt'})
 
